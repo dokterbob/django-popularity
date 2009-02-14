1b029b27f1dfadaf44dbfd6a990bdae1bd5be9d2
Mathijs de Bruin <mathijs@visualspace.nl>
2009-02-14 12:45:04 +0100

    Cleaned up the safe import code a bit.

--- demo/testapp/views.py
+++ demo/testapp/views.py
@@ -1,6 +1,11 @@
+import logging
+
 from django.conf import settings
 if 'popularity' in settings.INSTALLED_APPS:
-    POPULARITY = True
+    logging.debug('Django_popularity found and will be used.')
     from popularity.models.ViewTracker import add_view_for
 else:
-    POPULARITY = False
+    logging.warn('Django_popularity not found, creating a bogus function.'
+    'If popularity does not exist, create a bogus function.'
+    def add_view_for(*args, **kwargs):
+        pass
