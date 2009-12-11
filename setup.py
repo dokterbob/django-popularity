d42ce285eac580210a5c96abca30a5b456f8b3f4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2009-12-11 12:28:15 +0100

    Changed license to AGPL, because software is like sex: it's better when it's free.

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# This file is part of django-popularity.
+# 
+# django-popularity: A generic view- and popularity tracking pluggable for Django. 
+# Copyright (C) 2008-2009 Mathijs de Bruin
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from distutils.core import setup
 
 setup(
@@ -13,7 +31,7 @@ setup(
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
-                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'License :: OSI Approved :: GNU Affero General Public License v3',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
                    'Topic :: Utilities'],
