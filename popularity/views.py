900fa65bbad347485d215b113b188e07025f609f
Sebastien Fievet <sebastien@hyperweek.com>
2010-07-16 09:42:03 +0200

    Added AJAX handling with JSON. Now, returns the updated object.

--- popularity/views.py
+++ popularity/views.py
@@ -20,16 +20,25 @@ import logging
 
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
+from django.utils import simplejson
 
 from models import ViewTracker
 
 def add_view_for(request, content_type_id, object_id):
+    response_dict= {}
+
     ct = ContentType.objects.get(pk=content_type_id)
     myobject = ct.get_object_for_this_type(pk=object_id)
-    
+
     logging.debug('Adding view for %s through web.', myobject)
-    
+
     ViewTracker.add_view_for(myobject)
-    
+    tracker = ViewTracker.get_views_for(myobject)
+
+    response_dict.update({'success': True, 'tracker': tracker})
+
+    if request.is_ajax():
+        return HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+
     return HttpResponse()
     
