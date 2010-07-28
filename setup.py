8dbb71df8c23caca4cc25d8e3fe070b9a10e4016
Sebastien Fievet <sebastien@hyperweek.com>
2010-07-28 16:30:05 +0200

    Bumped version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except:
 
 setup(
     name = 'django-popularity',
-    version = "0.2.0",
+    version = "0.2.1",
     description = 'A generic view- and popularity tracking pluggable for Django.',
     long_description = README,
     author = 'Mathijs de Bruin',
