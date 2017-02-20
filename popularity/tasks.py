74025a241aa981a2e3cb5003e1914c27f6ff35bb
yeago <yeago999@gmail.com>
2017-02-20 11:46:21 -0800

    cleanup tasks

--- popularity/tasks.py
+++ popularity/tasks.py
@@ -7,8 +7,11 @@ from celery.decorators import task
 
 from popularity.models import ViewTracker
 
+log = logging.getLogger('__name__')
+
+
 @task
-def viewtrack(ct,pk,ip_address):
+def viewtrack(ct, pk, ip_address):
     '''IP address and caching are for situations where you want
         to not count a page refresh from the same location as another view.
         You can expire it to your specification, 300 (5 minutes) is pretty good though
@@ -17,13 +20,14 @@ def viewtrack(ct,pk,ip_address):
     instance = ct.model_class().objects.get(pk=pk)
 
     EXPIRE_TIME = getattr(settings, 'POPULARITY_VIEW_DELAY', 300)
-    if EXPIRE_TIME != False: # They expicitly don't want any delay
-        EXPIRE_KEY = 'popularity-view-%s-%s-%s' % (ct.pk, instance.pk, ip_address) 
+    if EXPIRE_TIME is not False:  # They expicitly don't want any delay
+        EXPIRE_KEY = u'popularity-view-%s-%s-%s' % (ct.pk, instance.pk, ip_address)
+        log.info(u"%s / %s / %s/ %s" % (EXPIRE_KEY, ct.pk, instance.pk, ip_address))
 
         if cache.get(EXPIRE_KEY):
-            logging.debug("Foregoing view, recent key found")
-            return # Don't add a view, they're revisiting too soon
+            log.info("Foregoing view, recent key found")
+            return  # Don't add a view, they're revisiting too soon
 
-        cache.set(EXPIRE_KEY, 1, EXPIRE_TIME) # 1 == just needs some value
+        cache.set(EXPIRE_KEY, 1, EXPIRE_TIME)  # 1 == just needs some value
 
     ViewTracker.add_view_for(instance)
